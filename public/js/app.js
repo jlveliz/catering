7b1746ef5d9d776d28a092670f544218252ddf0a
Jorge Veliz <jorgeconsalvacion@gmail.com>
2020-03-19 09:20:39 -0500

    dispone auth

--- public/js/app.js
+++ public/js/app.js
@@ -65721,8 +65721,8 @@ if (document.getElementById('example')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\laragon\www\catering\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\laragon\www\catering\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/php73/catering/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/php73/catering/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
