accfa8560481e98ceba161dc3d8e92b98770e9d4
Jorge Veliz <jorgeconsalvacion@gmail.com>
2020-03-20 10:10:44 -0500

    set user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,16 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'lastname' => $this->lastname,
+            'username' => $this->username,
+            'email' => $this->email,
+            'state' => $this->state,
+            'role' => $this->role,
+            'created_at' => $this->created_at,
+            'udpated_at' => $this->updated_at
+        ];
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -36,6 +36,8 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
     ];
 
 
