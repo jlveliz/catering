fe0c8c4f06436f31c6d879cec9d2f59919db6d5f
Jorge Veliz <jorgeconsalvacion@gmail.com>
2020-05-04 06:53:09 -0500

    fix npm pakages for ubuntu

--- public/js/app.js
+++ public/js/app.js
@@ -54110,8 +54110,8 @@ var store = new vuex__WEBPACK_IMPORTED_MODULE_1__["default"].Store({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\laragon\www\catering\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\laragon\www\catering\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/php73/catering/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/php73/catering/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
