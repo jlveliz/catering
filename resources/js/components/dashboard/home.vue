f990a4ed6db48a667afd6f8abf3b0f3f927fc1b9
Jorge Veliz <jorgeconsalvacion@gmail.com>
2020-05-09 20:15:44 -0500

    termina content  - crea slots de contenido

--- resources/js/components/dashboard/home.vue
+++ resources/js/components/dashboard/home.vue
@@ -1,19 +1,35 @@
 <template>
-  <div class="page-header card">
-      <h1>Soy Home o Dashboard</h1>
+  <div>
+    <content-header-component>
+      <b-col lg="8">
+        <div class="page-header-title">
+          <home-icon  class="bg-c-blue"></home-icon>
+          <div class="d-inline">
+            <h5>Escritorio</h5>
+            <span>Muestra datos relevantes del negocio</span>
+          </div>
+        </div>
+      </b-col>
+    </content-header-component>
+
+    <content-main-content-component>
+      <div class="card">
+        <div class="card-header">Dashboard</div>
+      </div>
+    </content-main-content-component>
   </div>
 </template>
 
 <script>
-
+import { HomeIcon } from "vue-feather-icons";
+import ContentHeaderComponent from "./../../layouts/parts/ContentHeaderComponent";
+import ContentMainContentComponent from "./../../layouts/parts/ContentMainContentComponent";
 export default {
   name: "Dashboard",
-  data() {
-    return {
-    };
-  },
-  methods: {
-
+  components: {
+    HomeIcon,
+    ContentHeaderComponent,
+    ContentMainContentComponent
   }
 };
 </script>
---
+++ resources/js/layouts/parts/ContentHeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-header card">
+      <b-row class="align-items-end">
+          <slot></slot>
+      </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ContentHeaderComponent'
+}
+</script>
+
+<style>
+
+</style>
